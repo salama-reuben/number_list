0652978feb1a505c0002eca34d575636d71e7bea
salama-reuben <salama.reuben@binghamuni.edu.ng>
2024-12-05 10:35:18 +0100

    git remote add origin https://github.com/salama-reuben/number_list.git git branch -M main git push -u origin main

---
+++ numbers in reverse .py
@@ -0,0 +1,12 @@
+number_list = [4,8,5,3,29,31,6,2,41,26]
+
+index = 0
+while number_list[index:]:
+    index += 1
+
+    number_index = index - 1
+
+for number in range(number_index, -1, -1):
+
+    print(number_list[number])
+     #for reversed without using reverse directly
